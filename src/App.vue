9593a1b039a1308aa251eb8b6b2c2ec0b2fdcbf3
Seitek2002 <seitekbegaliev@gmail.com>
2024-09-30 19:27:58 +0600

    bugs-solved

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <header>  
-    <header class="flex justify-between border-b border-slate-200 px-10 ">
+  <header class="flex justify-between border-b border-slate-200 px-10">
     <div class="flex items-center gap-4">
-      <img src="src/assets/logo.svg" alt="Logo" class="w-11" />
+      <img src="./assets/logo.svg" alt="Logo" class="w-11" />
       <div>
         <h2 class="font-bold text-xl uppercase">REACT SNEAKERS</h2>
         <p class="text-slate-400">магазин лучших кроссовок</p>
@@ -15,32 +14,25 @@ import { RouterLink, RouterView } from 'vue-router'
 
     <ul class="flex items-center gap-10">
       <li class="flex items-center gap-3 cursor-pointer">
-        <img src="src/assets/cart-icon.svg" alt="Cart" />
+        <img src="./assets/cart-icon.svg" alt="Cart" />
         <b>1205 рубл </b>
       </li>
       <li class="flex items-center gap-3 cursor-pointer">
-        <img src="/heart.svg" alt="Favorite" />
+        <!-- <img src="/heart.svg" alt="Favorite" /> -->
         <span>Закладки</span>
       </li>
       <li class="flex items-center gap-3 cursor-pointer">
-        <img src="/profile.svg" alt="Favorite" />
+        <!-- <img src="/profile.svg" alt="Favorite" /> -->
         <span>Профиль</span>
       </li>
     </ul>
   </header>
-  </header>
-  <h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-
+  <nav>
+    <RouterLink to="/">Home</RouterLink>
+    <RouterLink to="/about">About</RouterLink>
+  </nav>
 
   <RouterView />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 
-import'./index.css/'
+import './assets/index.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
